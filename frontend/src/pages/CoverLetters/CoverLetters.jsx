09d8dce37a74e6fb53d39fcde5653f47ecf817bd
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-10-11 20:32:36 +0530

    Add CoverLetters and ResumeTemplates components with basic structure

---
+++ frontend/src/pages/CoverLetters/CoverLetters.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './CoverLetters.css';
+
+const CoverLetters = () => {
+    return (
+        <div>Cover Letters</div>
+    )
+}
+
+export default CoverLetters;
---
+++ frontend/src/pages/ResumeTemplates/ResumeTemplates.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './ResumeTemplates.css';
+
+const ResumeTemplates = () => {
+    return (
+        <div>Resume Templates</div>
+    )
+}
+
+export default ResumeTemplates;
