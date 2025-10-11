d722ecbe8b3ef483b470a5e9c3f7f32a32d16504
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-10-11 17:32:54 +0530

    Update favicon link and enhance assets management

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,25 @@
 import logo from './logo.png';
+import cross_icon from './cross-icon.png';
+import app_store from './app_store.png';
+import facebook_icon from './facebook_icon.png';
+import fevicon from './favicon.png';
+import linkdin_icon from './linkedin_icon.png';
+import play_store from './play_store.png';
+import profile_icon from './profile_icon.png';
+import search_icon from './search_icon.png';
+import twitter_icon from './twitter_icon.png';
+import logo_animated from './logo_animated.gif';
 
 export const assets = {
-    logo
+    logo,
+    cross_icon,
+    app_store,
+    facebook_icon,
+    fevicon,
+    linkdin_icon,
+    play_store,
+    profile_icon,
+    search_icon,
+    twitter_icon,
+    logo_animated
 }
