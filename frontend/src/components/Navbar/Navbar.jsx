90bab1972dd38d3df7977932fd4caffd132c8537
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-10-11 19:28:46 +0530

    Refactor Navbar component structure and enhance styles for improved layout

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import './Navbar.css';
-import {assets} from '../../assets/assets.js'
+import { assets } from '../../assets/assets.js'
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <img src={assets.logo} alt="" className="logo"/>
-            <ul className="navbar-menu">
-                <li>Resume Templates</li>
-                <li>Cover Letters</li>
-            </ul>
-            <div className="navbar-right">
-                <button>Sign In</button>
+            <img src={assets.logo} alt="" className="logo" />
+            <div className="navbar-box">
+                <ul className="navbar-menu">
+                    <li>Resume Templates</li>
+                    <li>Cover Letters</li>
+                </ul>
+                <div className="navbar-right">
+                    <button>Sign In</button>
+                </div>
             </div>
+
         </div>
     )
-    
+
 }
 
 export default Navbar;
