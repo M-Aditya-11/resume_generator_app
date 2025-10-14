00e7bc253989e67cdc68c3eebd9cc414a1855063
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-10-14 21:24:28 +0530

    Refactor Header component layout and update text for clarity

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,15 +5,26 @@ import { assets } from '../../assets/assets.js'
 const Header = () => {
     return (
         <div className="header">
-            <div className="hero-top-left">
+            <div className="hero-top">
                 <div className="hero-top-left-heading">
                     <p>Build Resume</p>
                     <p>that</p>
-                    <p>get interviews</p>
+                    <p>get interviews.</p>
                 </div>
                 <div className="hero-top-right-image">
                     <img src={assets.hero_image_top} alt="" className="hero-top-image" />
                 </div>
+            </div>
+            <div className="hero-bottom">
+                <div className="hero-bottom-left-image">
+                    <img src={assets.hero_image_bottom} alt="" className="hero-bottom-image" />
+                </div>
+                <div className="hero-bottom-right-heading">
+                    <p>Grow your story</p>
+                    <p>with clean resumes</p>
+                    <p>and thoughtful templates.</p>
+                </div>
+
             </div>
         </div>
     )
