c46451e77ffe2f086ae6f5d0df2471b299c5bb86
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-10-14 22:55:16 +0530

    Add Footer component to Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Home.css';
 import Header from '../../components/Header/Header.jsx'
+import Footer from "../../components/Footer/Footer.jsx";
 
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     )
 }
